6dce48d8fb939cab42f6bf51b0f273e890a1a3a0
Fabian <fabianbarraez@gmail.com>
2021-11-17 09:54:49 -0500

    feat: 1st version of the server running

--- webserver/server.py
+++ webserver/server.py
@@ -3,20 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/<username>/<int:post_id>")
-def hello_world(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+@app.route("/index.html")
+def home():
+    return render_template('index.html')
+
+@app.route("/works.html")
+def works():
+    return render_template('works.html')
     
 @app.route("/about.html")
 def about():
     return render_template('about.html')
+    
+@app.route("/contact.html")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/components.html")
+def components():
+    return render_template('components.html')
 
 
-@app.route("/blog")
-def blog():
-    return "<p>These are my thoughts on blogs</p>"
-
-@app.route("/blog/2020/dogs")
-def blog2():
-    return "<p>This is my dog</p>"
 
